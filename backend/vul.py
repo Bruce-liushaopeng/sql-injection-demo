4d19ffa8a82362bd1b1904b32e8f45340ba7ba74
bruce <bruceliu1107@gmail.com>
2023-01-20 13:50:38 -0500

    delete main function in vul.py

--- backend/vul.py
+++ backend/vul.py
@@ -3,21 +3,7 @@ import sqlite3
 def newUserToDB(firstName, lastName):
     conn = sqlite3.connect('sample.db')
 
-    conn.execute(f"INSERT INTO USER ( FirstName, LastName) VALUES ('Bruce123', 'Liu111')")
-    conn.commit()
-
-    curser = conn.execute("SELECT FirstName, LastName from USER")
-
-    for row in curser:
-        print("ID = ", row[0])
-        print("FirstName = ", row[1])
-
-    conn.close()
-
-if __name__ == "__main__":
-    conn = sqlite3.connect('sample.db')
-
-    conn.execute(f"INSERT INTO USER ( FirstName, LastName) VALUES ('Bruce111', 'Liu111')")
+    conn.execute(f"INSERT INTO USER ( FirstName, LastName) VALUES ('{firstName}', '{lastName}')")
     conn.commit()
 
     curser = conn.execute("SELECT FirstName, LastName from USER")
