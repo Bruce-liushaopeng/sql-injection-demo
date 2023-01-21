9a497b2c96c3b0b0422491388346bba97e8d61b2
Tony Abou-Zeidan <tony.azp25@gmail.com>
2023-01-21 12:13:42 -0500

    Update.

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,33 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from vul import newUserToDB, getUserFromDB
+import vul_wrapper as v
+
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/addUser/<userName>/<password>', methods=['POST'], strict_slashes=False)
-def addUser(userName=None, password=None):
-    print(userName, password)
-    newUserToDB(userName, password)
+
+@app.route('/addUser/<username>/<password>', methods=['POST'], strict_slashes=False)
+def add_user(username=None, password=None):
+    """
+    Flask API function to add a user to the system.
+
+    :param username: The username of the user to add
+    :param password: The password of the user to add
+    :return: Redundant message
+    """
+
+    print(username, password)
+    v.add_user(username, password)
     return "ok"
 
-@app.route('/getUser/<userName>', methods=['GET'], strict_slashes=False)
-def getUser(userName=None):
-    respond = getUserFromDB(userName)
+
+@app.route('/getUser/<username>', methods=['GET'], strict_slashes=False)
+def get_user(username=None) -> list:
+    """
+    Flask API function to get a user from the system.
+
+    :param username: The username of the user to find
+    :return: The user information
+    """
+    respond = v.get_user(username)
     return respond
