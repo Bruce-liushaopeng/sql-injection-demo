2da7febe9f9987558ee6d9e364490144ebfda792
bruce <bruceliu1107@gmail.com>
2023-01-21 14:01:05 -0500

    merge in process

--- backend/vul.py
+++ backend/vul.py
@@ -1,16 +1,32 @@
 import sqlite3
 
-def newUserToDB(userName, password):
+
+def add_user_to_db(username: str, password: str) -> str:
+    """
+    Adds a new user to the database
+
+    :param username: The username for the user
+    :param password: The password of the user
+    :return: Add user message
+    """
     conn = sqlite3.connect('sample.db')
-    conn.execute(f"INSERT INTO USER ( userName, password) VALUES ('{userName}', '{password}')")
+    conn.execute(f"INSERT INTO USER ( userName, password) VALUES ('{username}', '{password}')")
     conn.commit()
     conn.close()
-    return "user ["+ userName +"] added auccess"
+    return "user [" + username + "] added auccess"
 
-def getUserFromDB(userName):
+
+def get_user_from_db(username: str) -> list:
+    """
+    Gets a user from the SQLite database.
+    This function is susceptible to SQL injection.
+
+    :param username: The username
+    :return: The user information
+    """
     conn = sqlite3.connect('sample.db')
-    print(userName)
-    curser = conn.execute(f"SELECT userName, password from USER where userName = '{userName}'")
+    print(username)
+    curser = conn.execute(f"SELECT userName, password from USER where userName = '{username}'")
     userInfo = []
     for row in curser:
         data = [row[0], row[1]]
